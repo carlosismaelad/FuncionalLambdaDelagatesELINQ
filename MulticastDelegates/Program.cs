9662ab0b86b5fb21c0e11dd34ee3d8f329e85f4c
carlosismaelad <carlosismaelad@outlook.com>
2024-01-18 19:00:20 -0300

    Entendendo o uso de Multicast Delegates

---
+++ MulticastDelegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using MulticastDelegates.Service;
+
+namespace MulticastDelegates
+{
+    delegate void BinaryNumbericOperation(double n1, double n2);
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // Multicast Delegates guardam referência para mais de um método;
+            // Pode-se usar o operador +=
+            // A chamada Invoke (ou sintaxe reduzida) executa todos os métodos
+            // na ordem em que foram adicionados
+            // Seu uso faz sentido para métodos void
+            double a = 10;
+            double b = 15;
+            BinaryNumbericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op(a, b);
+        }
+
+
+    }
+}
---
+++ MulticastDelegates/Service/CalculationService.cs
@@ -0,0 +1,17 @@
+namespace MulticastDelegates.Service
+{
+    public class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine(max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine(sum);
+        }
+    }
+}
