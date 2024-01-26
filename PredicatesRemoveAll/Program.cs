334015d78c2975782ab3fd634e3633c9d0d3f424
carlosismaelad <carlosismaelad@outlook.com>
2024-01-26 00:55:33 -0300

    Predicate como delegate

---
+++ PredicatesRemoveAll/Program.cs
@@ -0,0 +1,30 @@
+﻿using PredicatesRemoveAll.entities;
+
+namespace PredicatesRemoveAll
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
---
+++ PredicatesRemoveAll/entities/Product.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PredicatesRemoveAll.entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
+    }
+}
