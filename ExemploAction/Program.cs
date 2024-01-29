3b1024d18e463d1cf341396b8526caf8a9102a6a
carlosismaelad <carlosismaelad@outlook.com>
2024-01-29 14:45:16 -0300

    Action com referência a método estático

---
+++ ExemploAction/Program.cs
@@ -0,0 +1,31 @@
+﻿using ExemploAction.Entities;
+
+namespace ExemploAction
+{
+    class Program
+    {
+        public static void Main(string[] argas)
+        {
+            Console.Clear();
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}
