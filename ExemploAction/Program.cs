8f54d2a9c79b59ad11c3d8b0e1f8f1c80cf15949
carlosismaelad <carlosismaelad@outlook.com>
2024-01-29 22:57:49 -0300

    Action instanciando objeto do tipo delegate Action

--- ExemploAction/Program.cs
+++ ExemploAction/Program.cs
@@ -15,7 +15,9 @@ namespace ExemploAction
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            list.ForEach(UpdatePrice);
+            Action<Product> act = UpdatePrice;
+
+            list.ForEach(act);
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
