239be995cda4cc369df290284b8477188a24d1ac
carlosismaelad <carlosismaelad@outlook.com>
2024-01-30 00:05:23 -0300

    Select com referência a método static

---
+++ FunComSelect/Entities/Product.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+
+namespace ExemploAction.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
