090ffc54eaf66b9fb74ad81cd1dec6a7f8ceca83
carlosismaelad <carlosismaelad@outlook.com>
2024-01-29 23:01:09 -0300

    Expressão lambda em objeto do tipo delegate Action

--- ExemploAction/Program.cs
+++ ExemploAction/Program.cs
@@ -15,7 +15,7 @@ namespace ExemploAction
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = UpdatePrice;
+            Action<Product> act = p => { p.Price += p.Price * 0.1; };
 
             list.ForEach(act);
             foreach (Product p in list)
@@ -25,9 +25,9 @@ namespace ExemploAction
 
         }
 
-        static void UpdatePrice(Product p)
-        {
-            p.Price += p.Price * 0.1;
-        }
+        // static void UpdatePrice(Product p)
+        // {
+        //     p.Price += p.Price * 0.1;
+        // }
     }
 }
