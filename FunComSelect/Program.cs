58ac4a9b3bc038b518148e9108abad05515f90be
carlosismaelad <carlosismaelad@outlook.com>
2024-01-30 00:08:08 -0300

    Select com referencia a delegate declarado

--- FunComSelect/Program.cs
+++ FunComSelect/Program.cs
@@ -15,7 +15,9 @@ namespace FunComSelect
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            List<string> result = list.Select(NameUpper).ToList();
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
 
             foreach (string s in result)
             {
