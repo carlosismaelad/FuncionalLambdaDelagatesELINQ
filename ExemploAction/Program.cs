3c9598ac2328664c772c224107d1177979d6b76e
carlosismaelad <carlosismaelad@outlook.com>
2024-01-29 23:02:32 -0300

    Expressão lambda inline dentro do ForEach

--- ExemploAction/Program.cs
+++ ExemploAction/Program.cs
@@ -15,9 +15,9 @@ namespace ExemploAction
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Action<Product> act = p => { p.Price += p.Price * 0.1; };
+            // Action<Product> act = p => { p.Price += p.Price * 0.1; };
 
-            list.ForEach(act);
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
