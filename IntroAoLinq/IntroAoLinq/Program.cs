3f0176c264011e251ac545eefd6f71e467d2cced
carlosismaelad <carlosismaelad@outlook.com>
2024-02-11 15:17:26 -0300

    Introdução ao LINQ

---
+++ IntroAoLinq/IntroAoLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace IntroAoLinq
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            // Define the query expression
+            var result  = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Execute the query
+            foreach(int x in result) {  Console.WriteLine(x); }
+
+        }
+    }
+}
